c75117878091224fbf4437db268097688ffe4e09
Emirhan CİNCİ <78696056+EmirhanCinci@users.noreply.github.com>
2024-09-10 13:23:01 +0300

    Added performance monitoring aspect for method execution.

---
+++ Cintruct/Infrastructure/Aspects/PerformanceAspect.cs
@@ -0,0 +1,33 @@
+﻿using Castle.DynamicProxy;
+using Infrastructure.Utilities.Interceptors;
+using Infrastructure.Utilities.IoC;
+using System.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Aspects
+{
+    public class PerformanceAspect : MethodInterception
+    {
+        private readonly int _interval;
+        private readonly Stopwatch _stopwatch;
+        public PerformanceAspect(int interval)
+        {
+            _interval = interval;
+            _stopwatch = ServiceTool.ServiceProvider.GetService<Stopwatch>();
+        }
+
+        protected override void OnBefore(IInvocation invocation)
+        {
+            _stopwatch.Start();
+        }
+
+        protected override void OnAfter(IInvocation invocation)
+        {
+            if (_stopwatch.Elapsed.TotalSeconds > _interval)
+            {
+                Debug.WriteLine($"Performans Durumu: {invocation.Method.DeclaringType?.FullName}.{invocation.Method.Name} --> {_stopwatch.Elapsed.TotalSeconds}");
+            }
+            _stopwatch.Reset();
+        }
+    }
+}
