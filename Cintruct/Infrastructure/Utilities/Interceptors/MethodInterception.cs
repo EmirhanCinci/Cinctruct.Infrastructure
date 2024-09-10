50833a2d6775bdf2ca7b6bbb1932a27d41b81853
Emirhan CİNCİ <78696056+EmirhanCinci@users.noreply.github.com>
2024-09-10 11:05:20 +0300

    Added basic interceptor classes for AOP.

---
+++ Cintruct/Infrastructure/Utilities/Interceptors/MethodInterception.cs
@@ -0,0 +1,59 @@
+﻿using Castle.DynamicProxy;
+
+namespace Infrastructure.Utilities.Interceptors
+{
+    public class MethodInterception : MethodInterceptionBaseAttribute
+    {
+        protected virtual void OnBefore(IInvocation invocation)
+        {
+
+        }
+
+        protected virtual void OnAfter(IInvocation invocation)
+        {
+
+        }
+
+        protected virtual void OnException(IInvocation invocation, Exception e)
+        {
+
+        }
+
+        protected virtual void OnSuccess(IInvocation invocation)
+        {
+
+        }
+
+        public override void Intercept(IInvocation invocation)
+        {
+            var isSuccess = true;
+            OnBefore(invocation);
+            try
+            {
+                invocation.Proceed();
+                if (invocation.ReturnValue is Task returnValueTask)
+                {
+                    returnValueTask.GetAwaiter().GetResult();
+                }
+                if (invocation.ReturnValue is Task task && task.Exception != null)
+                {
+                    throw task.Exception;
+                }
+            }
+            catch (Exception exception)
+            {
+                isSuccess = false;
+                OnException(invocation, exception);
+                throw;
+            }
+            finally
+            {
+                if (isSuccess)
+                {
+                    OnSuccess(invocation);
+                }
+                OnAfter(invocation);
+            }
+        }
+    }
+}
