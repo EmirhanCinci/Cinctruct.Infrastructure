57f8f42e98a9b95f0b04d43e284f08e5f35a321b
Emirhan CİNCİ <78696056+EmirhanCinci@users.noreply.github.com>
2024-09-10 13:52:44 +0300

    Added BaseController with custom response handling.

---
+++ Cintruct/Infrastructure/WebApi/Controllers/BaseController.cs
@@ -0,0 +1,16 @@
+﻿using Infrastructure.Utilities.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Infrastructure.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        [NonAction]
+        public IActionResult SendResponse<T>(CustomApiResponse<T> response) where T : class
+        {
+            return new ObjectResult(response) { StatusCode = response.StatusCode };
+        }
+    }
+}
