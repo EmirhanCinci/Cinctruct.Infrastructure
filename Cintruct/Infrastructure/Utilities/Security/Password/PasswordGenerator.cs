187f3bdbd6bbc062c059393dac921778ba4f205b
Emirhan CİNCİ <78696056+EmirhanCinci@users.noreply.github.com>
2024-09-10 10:56:52 +0300

    Added password and security key utilities.

---
+++ Cintruct/Infrastructure/Utilities/Security/Password/PasswordGenerator.cs
@@ -0,0 +1,22 @@
+﻿using Infrastructure.Constants;
+using System.Security.Cryptography;
+
+namespace Infrastructure.Utilities.Security.Password
+{
+    public class PasswordGenerator
+    {
+        private static readonly char[] Characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()".ToCharArray();
+
+        public static string GeneratePassword(int length = 8)
+        {
+            if (length < 1) throw new ArgumentException(SystemMessages.InvalidLengthPassword);
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                var byteBuffer = new byte[length];
+                rng.GetBytes(byteBuffer);
+                var passwordChars = byteBuffer.Select(b => Characters[b % Characters.Length]).ToArray();
+                return new string(passwordChars);
+            }
+        }
+    }
+}
---
+++ Cintruct/Infrastructure/Utilities/Security/Password/PasswordHelper.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Infrastructure.Utilities.Security.Password
+{
+    public static class PasswordHelper
+    {
+        public static void CreatePasswordHashByHmacSha512(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        public static bool VerifyPasswordHashByHmacSha512(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] != passwordHash[i])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public static (byte[], byte[]) CreatePasswordByHmacSha512(string password)
+        {
+            byte[] passwordHash, passwordSalt;
+            CreatePasswordHashByHmacSha512(password, out passwordHash, out passwordSalt);
+            return (passwordHash, passwordSalt);
+        }
+    }
+}
