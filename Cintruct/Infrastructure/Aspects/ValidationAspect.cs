1216383ee90fe273aec31707d2e2f53bd0888df9
Emirhan CİNCİ <78696056+EmirhanCinci@users.noreply.github.com>
2024-09-10 13:21:19 +0300

    Added validation aspect and tool for method interception.

---
+++ Cintruct/Infrastructure/Aspects/ValidationAspect.cs
@@ -0,0 +1,32 @@
+﻿using Castle.DynamicProxy;
+using FluentValidation;
+using Infrastructure.Constants;
+using Infrastructure.CrossCuttingConcerns.Validations;
+using Infrastructure.Utilities.Interceptors;
+
+namespace Infrastructure.Aspects
+{
+    public class ValidationAspect : MethodInterception
+    {
+        private readonly Type _validatorType;
+        public ValidationAspect(Type validatorType)
+        {
+            if (!typeof(IValidator).IsAssignableFrom(validatorType))
+            {
+                throw new Exception(SystemMessages.InvalidValidationClass);
+            }
+            _validatorType = validatorType;
+        }
+
+        protected override void OnBefore(IInvocation invocation)
+        {
+            var validator = (IValidator)Activator.CreateInstance(_validatorType);
+            var entityType = _validatorType?.BaseType?.GetGenericArguments()[0];
+            var entities = invocation.Arguments.Where(t => t.GetType() == entityType);
+            foreach (var entity in entities)
+            {
+                ValidationTool.Validate(validator, entity);
+            }
+        }
+    }
+}
---
+++ Cintruct/Infrastructure/CrossCuttingConcerns/Validations/ValidationTool.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace Infrastructure.CrossCuttingConcerns.Validations
+{
+    public static class ValidationTool
+    {
+        public static void Validate(IValidator validator, object entity)
+        {
+            var context = new ValidationContext<object>(entity);
+            var result = validator.Validate(context);
+            if (!result.IsValid)
+            {
+                var message = string.Join(" - ", result.Errors.Select(error => $"{error.ErrorMessage}"));
+                throw new ValidationException(message);
+            }
+        }
+    }
+}
