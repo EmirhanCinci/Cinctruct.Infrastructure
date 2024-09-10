03d3dbf2ebcdb7e9f25fc3339702d591c3294721
Emirhan CİNCİ <78696056+EmirhanCinci@users.noreply.github.com>
2024-09-10 13:51:04 +0300

    Added file handling functionality with FileHelper and FileDto.

---
+++ Cintruct/Infrastructure/Model/Dtos/Implementations/FileDto.cs
@@ -0,0 +1,11 @@
+﻿using Infrastructure.Model.Dtos.Interfaces;
+
+namespace Infrastructure.Model.Dtos.Implementations
+{
+    public class FileDto : IDto
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Base64String { get; set; } = string.Empty;
+        public string ContentType { get; set; } = string.Empty;
+    }
+}
---
+++ Cintruct/Infrastructure/Utilities/Files/FileHelper.cs
@@ -0,0 +1,31 @@
+﻿using Infrastructure.Model.Dtos.Implementations;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Utilities.Files
+{
+    public class FileHelper
+    {
+        private readonly IConfiguration _configuration;
+        public FileHelper(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string SaveFile(FileDto fileDto, string mainFilePath)
+        {
+            byte[] fileBytes = Convert.FromBase64String(fileDto.Base64String);
+            string fileExtension = Path.GetExtension(fileDto.Name);
+            string uniqueFileName = Guid.NewGuid().ToString() + fileExtension;
+            string filePath = Path.Combine("files", mainFilePath, uniqueFileName);
+            string directoryPath = Path.GetDirectoryName(filePath);
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+            File.WriteAllBytes(filePath, fileBytes);
+            string baseUrl = _configuration.GetSection("FileSettings:BaseUrl").Value;
+            string fileUrl = new Uri(new Uri(baseUrl), $"/Files/{mainFilePath}/{uniqueFileName}").ToString();
+            return fileUrl;
+        }
+    }
+}
