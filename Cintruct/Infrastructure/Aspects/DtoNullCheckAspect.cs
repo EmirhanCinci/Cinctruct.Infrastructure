1036e3f4f93a511653a015d8f34febe6066aac7f
Emirhan CİNCİ <78696056+EmirhanCinci@users.noreply.github.com>
2024-09-10 13:11:56 +0300

    Added aspects for DTO null checks and ID validation.

---
+++ Cintruct/Infrastructure/Aspects/DtoNullCheckAspect.cs
@@ -0,0 +1,19 @@
+﻿using Castle.DynamicProxy;
+using Infrastructure.Constants;
+using Infrastructure.CrossCuttingConcerns.Exceptions;
+using Infrastructure.Utilities.Interceptors;
+
+namespace Infrastructure.Aspects
+{
+    public class DtoNullCheckAspect : MethodInterception
+    {
+        protected override void OnBefore(IInvocation invocation)
+        {
+            object arg = invocation.Arguments[0];
+            if (arg == null)
+            {
+                throw new BadRequestException(SystemMessages.RequiredModel);
+            }
+        }
+    }
+}
---
+++ Cintruct/Infrastructure/Aspects/IdCheckAspect.cs
@@ -0,0 +1,43 @@
+﻿using Castle.DynamicProxy;
+using Infrastructure.Constants;
+using Infrastructure.CrossCuttingConcerns.Exceptions;
+using Infrastructure.Utilities.Interceptors;
+
+namespace Infrastructure.Aspects
+{
+    public class IdCheckAspect : MethodInterception
+    {
+        protected override void OnBefore(IInvocation invocation)
+        {
+            object arg = invocation.Arguments[0];
+            if (arg == null)
+            {
+                throw new BadRequestException(SystemMessages.NotEmptyId);
+            }
+            if (arg is long longValue)
+            {
+                CheckId(longValue);
+            }
+            else if (arg is int intValue)
+            {
+                CheckId(intValue);
+            }
+            else if (arg is short shortValue)
+            {
+                CheckId(shortValue);
+            }
+            else if (arg is byte byteValue)
+            {
+                CheckId(byteValue);
+            }
+        }
+
+        private void CheckId(long id)
+        {
+            if (id <= 0)
+            {
+                throw new BadRequestException(SystemMessages.IdGreaterThanZero);
+            }
+        }
+    }
+}
