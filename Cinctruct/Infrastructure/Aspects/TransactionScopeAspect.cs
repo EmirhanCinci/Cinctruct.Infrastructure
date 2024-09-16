aa0a4bc7ecad6bbee362c0d6df8fd0841239a672
Emirhan CİNCİ <78696056+EmirhanCinci@users.noreply.github.com>
2024-09-16 13:51:17 +0300

    Added TransactionScopeAspect for transaction management.

---
+++ Cinctruct/Infrastructure/Aspects/TransactionScopeAspect.cs
@@ -0,0 +1,43 @@
+﻿using Castle.DynamicProxy;
+using Infrastructure.Utilities.Interceptors;
+using System.Transactions;
+
+namespace Infrastructure.Aspects
+{
+	/// <summary>
+	/// AOP TransactionScopeAspect class to handle transaction management using the TransactionScope.
+	/// Automatically manages transactions, ensuring that changes are committed or rolled back depending on success or failure.
+	/// </summary>
+	public class TransactionScopeAspect : MethodInterception
+	{
+		/// <summary>
+		/// Intercepts the method execution to apply transaction management logic.
+		/// </summary>
+		/// <param name="invocation">Method invocation information.</param>
+		public override void Intercept(IInvocation invocation)
+		{
+			using (TransactionScope transactionScope = new TransactionScope())
+			{
+				try
+				{
+					invocation.Proceed();
+					if (invocation.ReturnValue is Task returnValueTask)
+					{
+						returnValueTask.GetAwaiter().GetResult();
+					}
+
+					if (invocation.ReturnValue is Task task && task.Exception != null)
+					{
+						throw task.Exception;
+					}
+					transactionScope.Complete();
+				}
+				catch (Exception)
+				{
+					transactionScope.Dispose();
+					throw;
+				}
+			}
+		}
+	}
+}
